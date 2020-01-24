2d60ea63450c12aaa814ac3f6693c1fbe4309a29
Glen <ultrapancake@gmail.com>
2020-01-24 15:43:35 -0700

    added logic for post route api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,6 +6,34 @@ module.exports = function(app) {
   });
   app.post("/api/friends", function(req, res) {
     //insert logic here that handles compatability
-    res.json();
+    const userData = req.body;
+    const userScore = userData.scores;
+    let bestMatch = {
+      name: "",
+      photo: "",
+      friendDiff: Infinity
+    };
+
+    let totalDifference;
+
+    for (let i = 0; i < friends.length; i++) {
+      const currentFriend = friends[i];
+      totalDifference = 0;
+      for (let j = 0; j < currentFriend.scores.length; j++) {
+        const currentFriendScore = currentFriend.scores[j];
+        totalDifference += Math.abs(
+          parseInt(userScore[j]) - parseInt(currentFriendScore)
+        );
+      }
+      if (totalDifference <= bestMatch.friendDiff) {
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDiff = totalDifference;
+      }
+    }
+
+    friends.push(userData);
+
+    res.json(bestMatch);
   });
 };
