bb5caca74542832376157303c1f8a03cfc3d1708
Glen <ultrapancake@gmail.com>
2020-01-24 15:06:38 -0700

    file structure and initial creation of public folders

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,11 @@
+const friends = require("../data/friends");
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+  app.post("/api/friends", function(req, res) {
+    //insert logic here that handles compatability
+    res.json();
+  });
+};
